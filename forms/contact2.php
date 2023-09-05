fc6e0bbae3628ac6b89a49bd2203f6533b27f209
Ravi Chaudhary <raraviattri@gmail.com>
2023-09-05 12:09:01 +0530

    commit form

---
+++ forms/contact2.php
@@ -0,0 +1,50 @@
+<div class="container tpmgn">
+                <div class="row">
+                    <h1 class="banerbotm">Contact</h1>
+                    
+                    <div class="col-md-2"></div>
+                    <div class="col-md-8">
+                        <div class="getin">
+                        <form  method="post" action="submit.php" enctype="multipart/form-data" class="wpcf7-form init">
+                            
+                          <div class="form-group row">
+                            <label for="staticEmail" class="col-sm-2 col-form-label">Name</label>
+                            <div class="col-sm-10">
+                              <input type="text" name="name" class="form-control" required>
+                            </div>
+                          </div>
+                          
+                                             
+                          <div class="form-group row">
+                            <label for="inputPassword" class="col-sm-2 col-form-label">E-mail</label>
+                            <div class="col-sm-10">
+                              <input type="email" name="email"class="form-control"  required>
+                            </div>
+                          </div>
+                          
+                          <div class="form-group row">
+                            <label for="inputPassword" class="col-sm-2 col-form-label">Subject</label>
+                            <div class="col-sm-10">     
+                            <input type="text" name="sub" class="form-control"  required>
+
+                            </div>
+                          </div>
+
+                          <div class="form-group row">
+                            <label for="inputPassword" class="col-sm-2 col-form-label">Message</label>
+                            <div class="col-sm-10">     
+                            <input type="text" name="msg" class="form-control"  required>
+
+                            </div>
+                          </div>
+                          
+                          <input type="submit" name="submit2" value="Submit" class="form-control">
+                          
+                        </form>
+                        
+                        </div>
+                    </div>
+                    <div class="col-md-2"></div>
+
+                </div>
+            </div>
